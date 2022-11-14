10c5d3e9706148366f23fa992f195a204e84b349
SemenBogdanov <SemenBogdanov1985@yandex.ru>
2022-11-14 18:32:27 +0300

    Fix bug with find a pers by surname

--- my_functions/other_functions.py
+++ my_functions/other_functions.py
@@ -32,6 +32,7 @@ async def remind_me(wait_time=20):
             # print("не удалось доставить напоминание")
             # print(e)
 
+
 async def remind_cnp(wait_time=20):
     # today = str(datetime.datetime.today().date())
 
